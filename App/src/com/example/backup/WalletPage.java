f6ff33f4de9cef672e0ee119d24d7310b714d61f
rho <rohita323@gmail.com>
2014-07-18 23:30:00 +0530

    priliminary wallet page

--- App/src/com/example/backup/WalletPage.java
+++ App/src/com/example/backup/WalletPage.java
@@ -1,34 +1,18 @@
 package com.example.backup;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import com.example.backup.Views.TitleBar;
-import com.example.backup.backgroundtasks.LockScreenService;
-import com.example.backup.backgroundtasks.MyService;
-import com.example.backup.constants.Constants;
-import com.haibison.android.lockpattern.LockPatternActivity;
-import com.haibison.android.lockpattern.util.Settings;
-
 import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.Switch;
 import android.widget.TextView;
+
+import com.example.backup.Views.TitleBar;
+import com.example.backup.constants.Constants;
 
 public class WalletPage extends Activity implements Constants {
 
@@ -40,15 +24,11 @@ public class WalletPage extends Activity implements Constants {
 	private int Money_amount = 0;
 	private int tranfer_amount = 0;
 
-	private ImageView facebook;
-	private ImageView googleplus;
-	private ImageView linkedin;
-	private ImageView twitter;
-	private Button butt_money_transfer;
-	private Button butt_bank_transfer;
-	private TextView transfer_share;
-	private TextView total_amount;
-
+	private ImageView facebook, googleplus, linkedin, twitter;
+	private Button butt_money_transfer, butt_bank_transfer, butt_mob_sub,
+			butt_bank_sub;
+	private TextView transfer_share, total_amount;
+	private EditText Account_name, Account_number, Amount, IFSC;
 	boolean isDown_mobile = false;
 	boolean isDown_bank = false;
 
@@ -58,14 +38,8 @@ public class WalletPage extends Activity implements Constants {
 		setContentView(R.layout.wallet);
 
 		initTitle();
-
-		facebook = (ImageView) findViewById(R.id.im_fb);
-		googleplus = (ImageView) findViewById(R.id.im_gplus);
-		linkedin = (ImageView) findViewById(R.id.im_lin);
-		twitter = (ImageView) findViewById(R.id.im_tw);
-		butt_money_transfer = (Button) findViewById(R.id.Mob_Rech);
-		butt_bank_transfer = (Button) findViewById(R.id.Bank_Trans);
-
+		findTheViews();
+		
 		set_on_clicks();
 
 	}
@@ -87,27 +61,54 @@ public class WalletPage extends Activity implements Constants {
 		s_searchIcon.setEnabled(false);
 	}
 
+	private void findTheViews() {
+
+		facebook = (ImageView) findViewById(R.id.im_fb);
+		googleplus = (ImageView) findViewById(R.id.im_gplus);
+		linkedin = (ImageView) findViewById(R.id.im_lin);
+		twitter = (ImageView) findViewById(R.id.im_tw);
+		butt_money_transfer = (Button) findViewById(R.id.Mob_Rech);
+		butt_bank_transfer = (Button) findViewById(R.id.Bank_Trans);
+		butt_mob_sub = (Button) findViewById(R.id.submit);
+		butt_bank_sub =  (Button) findViewById(R.id.Bank_submit);
+		
+		
+	}
+
 	private void set_on_clicks() {
 
-		facebook.setOnClickListener(new OnClickListener() {
+//		facebook.setOnClickListener(new OnClickListener() {
+//			public void onClick(View arg0) {
+//			}
+//		});
+//
+//		googleplus.setOnClickListener(new OnClickListener() {
+//			public void onClick(View arg0) {
+//			}
+//		});
+//
+//		linkedin.setOnClickListener(new OnClickListener() {
+//			public void onClick(View arg0) {
+//			}
+//		});
+//
+//		twitter.setOnClickListener(new OnClickListener() {
+//			public void onClick(View arg0) {
+//			}
+//		});
+		
+		butt_bank_sub.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
+				submit(1);				
 			}
 		});
-
-		googleplus.setOnClickListener(new OnClickListener() {
+		
+		butt_mob_sub.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
+				submit(0);
 			}
 		});
-
-		linkedin.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-			}
-		});
-
-		twitter.setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-			}
-		});
+		
 
 		butt_money_transfer.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
@@ -119,15 +120,15 @@ public class WalletPage extends Activity implements Constants {
 								160, 66));
 						findViewById(R.id.Details).setVisibility(View.VISIBLE);
 						isDown_mobile = true;
-						
-						if(isDown_bank==true)
-						{
-							butt_bank_transfer.setBackgroundColor(Color.rgb(144, 200,84));
-							isDown_bank=false;
-							findViewById(R.id.Detail_bank).setVisibility(View.GONE);
+
+						if (isDown_bank == true) {
+							butt_bank_transfer.setBackgroundColor(Color.rgb(
+									144, 200, 84));
+							isDown_bank = false;
+							findViewById(R.id.Detail_bank).setVisibility(
+									View.GONE);
 						}
-						
-						
+
 					} else {
 						butt_money_transfer.setBackgroundColor(Color.rgb(144,
 								200, 84));
@@ -141,23 +142,23 @@ public class WalletPage extends Activity implements Constants {
 		});
 		butt_bank_transfer.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
-				
+
 				if (butt_bank_transfer.isPressed()) {
-					
+
 					if (isDown_bank == false) {
 						butt_bank_transfer.setBackgroundColor(Color.rgb(109,
 								160, 66));
-						findViewById(R.id.Detail_bank).setVisibility(View.VISIBLE);
+						findViewById(R.id.Detail_bank).setVisibility(
+								View.VISIBLE);
 						isDown_bank = true;
-						
-						if(isDown_mobile==true)
-						{
-							butt_money_transfer.setBackgroundColor(Color.rgb(144, 200,84));
+
+						if (isDown_mobile == true) {
+							butt_money_transfer.setBackgroundColor(Color.rgb(
+									144, 200, 84));
 							findViewById(R.id.Details).setVisibility(View.GONE);
-							isDown_mobile=false;
+							isDown_mobile = false;
 						}
-						
-						
+
 					} else {
 						butt_bank_transfer.setBackgroundColor(Color.rgb(144,
 								200, 84));
@@ -172,6 +173,24 @@ public class WalletPage extends Activity implements Constants {
 
 	}
 
+	protected void submit(int x){
+		
+		if (x==0) {
+		
+			Account_name = (EditText) findViewById(R.id.IAccN);
+			Account_number = (EditText) findViewById(R.id.IAccNu);
+			IFSC = (EditText) findViewById(R.id.IIFSC);
+			Amount = (EditText) findViewById(R.id.IAmt);
+		}
+			
+		if(x==1){
+			Account_number = (EditText) findViewById(R.id.Bank_IAccN);
+			Amount = (EditText) findViewById(R.id.Bank_IAmt);
+			IFSC = (EditText) findViewById(R.id.Bank_IIfsc);
+		}
+		
+	}
+	
 	protected void onDestroy() {
 		super.onDestroy();
 	};
